fdc2639b02f42a44b475725b24fd19c5e1f96353
ajw116 <andyjameswelsh@gmail.com>
2016-10-22 16:11:47 +1300

    athistime

--- layers/LakesOtago.js
+++ layers/LakesOtago.js
@@ -1,4 +1,4 @@
-var json_LakesOtago0={
+var geojson_LakesOtago = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
--- layers/OtagoRC.js
+++ layers/OtagoRC.js
@@ -1,4 +1,4 @@
-var json_OtagoRC1={
+var geojson_OtagoRC = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
